93764af1cba596e6304ac6ef64a0d1d2b8072d4b
gquirarte9 <74148103+gquirarte9@users.noreply.github.com>
2020-11-11 15:09:54 -0500

    Update buildQR.cpp

--- buildQR.cpp
+++ buildQR.cpp
@@ -3,4 +3,17 @@
 void buildQR(double A[ARRAYSIZE][ARRAYSIZE], double Q[ARRAYSIZE][ARRAYSIZE], double R[ARRAYSIZE][ARRAYSIZE]){
 	//takes A and decomposes it into Q and R, with QR = A
 	//...
+	
+	//Given matrix A: If nonsymmetric balance and reduce to Hessenberg Form 
+	
+	//Balancing: Reduce sensitivity of eigenvalues to rounding errors during the execution 
+	// (write balance)
+	
+	// Reduction to Hessenberg form by applying the elimination method (analogous to Gaussian elimination with pivoting)
+	//  (upper Hessenberg form 
+	
+	
+	//Taking an upper Hessenberg form matrix and solve for eigenvalues 
+	
+	
 }
