53b7fb174304db5cc618ef5ad61c860647fa16b8
gquirarte9 <74148103+gquirarte9@users.noreply.github.com>
2020-11-14 12:02:59 -0500

    Update Balance.cpp

--- Balance.cpp
+++ Balance.cpp
@@ -1,1 +1,7 @@
-void Balance(Array )
+
+void Balance(double A[ARRAYSIZE][ARRAYSIZE]){
+  //This function takes in a matrix A and then outputs a balanced matrix with idential eigenvalues 
+  // (Reduces sensitivity of eigenvalues to rounding errors) 
+  
+  
+}
