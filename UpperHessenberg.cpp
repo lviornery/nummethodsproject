b98678d0b7474a61cbe87bb2c7794e2472bf0e98
lviornery <lviornery@hmc.edu>
2020-11-14 18:42:38 -0500

    removed extra cpp file

--- UpperHessenberg.cpp
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-
-void UpperHessenberg(double A[ARRAYSIZE][ARRAYSIZE]){
-  //Takes in a real, nonsymmetric matrix A and applies the elimination method to reduce the matrix to an upper Hessenberg form with identical eigenvalues 
-  // 
-
-
-
-}
