29b49e5ea70e90c611f40a4189a9840e8afd0d4e
lviornery <lviornery@hmc.edu>
2020-11-14 18:52:18 -0500

    removed pointless variable

--- Balance.cpp
+++ Balance.cpp
@@ -2,8 +2,6 @@
 #include "matrixFunctions.h"
 
 void balance(double A[ARRAYSIZE][ARRAYSIZE]){
-  //This function takes in a matrix A and then outputs a balanced matrix with identical eigenvalues 
-  // (Reduces sensitivity of eigenvalues to rounding errors) 
-  
-  
+	//This function takes in a matrix A and then outputs a balanced matrix with identical eigenvalues 
+	// (Reduces sensitivity of eigenvalues to rounding errors)
 }
