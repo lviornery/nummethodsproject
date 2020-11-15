df3d0589142edf5c781bc8f11f409d3d4b7a0919
gquirarte9 <74148103+gquirarte9@users.noreply.github.com>
2020-11-15 12:31:31 -0500

    Update householderHessenberg.cpp

--- householderHessenberg.cpp
+++ householderHessenberg.cpp
@@ -3,4 +3,19 @@
 
 void householderHessenberg(double A[ARRAYSIZE][ARRAYSIZE]){
 	//This function takes in a matrix A and turns it into a Hessenberg matrix using Householder reflectors
+	int i,j, k; //Counters for iterations below
+	double p, scaling, hh, h, g, f;
+	
+	//pseudo-code
+	//for (i = 1; i <= ARRAYSIZE-2 ; i++) {
+	//	p = reflector(A(i+1:end,i));
+	//      A(i+1:ARRAYSIZE, i:ARRAYSIZE) = A(:, i+1: ARRAYSIZE) - 2*p*(p'*A(i+1:ARRAYSIZE,i:ARRAYSIZE));
+	//      A(:, i+1:ARRAYSIZE) = A(:, i+1:ARRAYSIZE) -2*(A(:,i+1:ARRAYSIZE)*p)*p';
+	//      A(i+2: ARRAYSIZE, i) = 0;
+	// end 
+		
+		
+	}
+	
+	//
 }
