ef7e387af8477a5a5de7b31d307491fa188213cd
gquirarte9 <74148103+gquirarte9@users.noreply.github.com>
2020-11-21 16:14:38 -0500

    Update householderHessenberg.cpp

--- householderHessenberg.cpp
+++ householderHessenberg.cpp
@@ -17,19 +17,20 @@ void householderHessenberg(double A[ARRAYSIZE][ARRAYSIZE]){
 	for (i = 1; i <= ARRAYSIZE-2 ; i++) {
 		
 		// Householder reflection unit vector u
-		x = A(i+1:ARRAYSIZE, i);
-		m = max(fabs(x));
+		x = A((i+1):ARRAYSIZE, i);
+		m = fmax(fabs(x));
 		u = x/m;
-		if u(1) ==0
+		if (u(i) ==0)
 			p = 1;
-		elseif u(1)> 0
+		elseif (u(i)> 0)
 			p = 1;
 		else
 			p = -1; 
 		end
-		u(1) = u(1) +p*norm(u);
+		u(i) = u(i) +p*norm(u);
 		u = u/norm(u);
 		u = u(:);
+		//Fixing syntax errors 
 	      A(i+1:ARRAYSIZE, i:ARRAYSIZE) = A(:, i+1: ARRAYSIZE) - 2*u*(u'*A(i+1:ARRAYSIZE,i:ARRAYSIZE)); //Implementing the householder reflector 
 	      A(:, i+1:ARRAYSIZE) = A(:, i+1:ARRAYSIZE) -2*(A(:,i+1:ARRAYSIZE)*u)*u';
 	      A(i+2: ARRAYSIZE, i) = 0;
