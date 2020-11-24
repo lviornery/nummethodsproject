2d06328094b1f95b9af6c8a53dac90a40b0f6e94
lviornery <lviornery@hmc.edu>
2020-11-24 11:48:11 -0500

    fixed imaginary eigenvalue generation

--- genMatrices.m
+++ genMatrices.m
@@ -4,8 +4,10 @@ mmat = diag(mdiag);
 kdiag = 5*rand(ARRAYSIZE,1); % all positive numbers between 0 and 5
 kmat = diag(kdiag);
 kod = 5*(rand(ARRAYSIZE,ARRAYSIZE)-.5); %off-diagonal elements of k are random
+kod = (kod+kod'); %make the k matrix symetric
 kod = kod - diag(diag(kod)); %set diagonal elements of off-diagonal matrix to 0
 kmat = kmat + kod;
 writematrix(mmat,'mmat.csv')
 writematrix(kmat,'kmat.csv')
 cond(inv(mmat)*kmat)
+eig(inv(mmat)*kmat)
